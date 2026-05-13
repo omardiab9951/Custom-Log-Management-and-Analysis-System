536a7d994fdc96884436a87cff189f10e009909a
Omar Nasr <omarnasr9975@gmail.com>
2026-05-13 16:29:53 +0300

    fixed a path issue

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # scripts/init_db.sh - Initializes the SQLite database using schema.sql
-DB="../logs_db.sqlite"
-
+DB="logs_db.sqlite"
 if [ -f "$DB" ]; then
     echo "✅ Database already exists: $DB"
 else
-    sqlite3 "$DB" < schema.sql
+    sqlite3 "$DB" < "$(dirname "$0")/schema.sql"
     echo "✅ Database created successfully: $DB"
 fi
